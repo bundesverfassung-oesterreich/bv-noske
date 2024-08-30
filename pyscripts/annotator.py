7031a8d156434bc85da0cfcad575e942b07029e7
cfhaak <c.f.haak@posteo.de>
2024-08-30 16:06:35 +0200

    tidied script, added morphanalysis

--- pyscripts/annotator.py
+++ pyscripts/annotator.py
@@ -1,7 +1,6 @@
 # needs: python -m spacy download de_dep_news_trf
 import de_dep_news_trf
 import glob
-import lxml.etree as ET
 import lxml.builder as builder
 from acdh_tei_pyutils.tei import TeiReader
 
@@ -14,32 +13,48 @@ teiMaker = builder.ElementMaker(
 )
 nl_processor = de_dep_news_trf.load()
 
-# iterate over docs
+
 data_dir = "./data"
 editions_dir = f"{data_dir}/editions"
 output_dir = f"{data_dir}/tokenized_xml"
+source_elements_xpath = "//tei:body//tei:p|//tei:body//tei:head|//tei:body//tei:item"
 
-for docpath in glob.glob(f"{editions_dir}/*.xml"):
-    # load doc
-    print(f"processing {docpath}")
-    xml_doc = TeiReader(docpath)
-    # identifiy text to annotate: source elements (p, head, item)
-    for source_element in xml_doc.any_xpath("//tei:body//tei:p|//tei:body//tei:head|//tei:body//tei:item"):
-        if source_element.text is not None:
-            tokens = nl_processor(source_element.text)
-            source_element.text = "\n"
-            for token in tokens:
-                subel_name = "w" if token.pos_ != "PUNCT" else "pc"
-                subel = teiMaker(
-                    subel_name,
-                    token.text,
-                    pos=token.pos_,
-                    lemma=token.lemma_
-                )
-                source_element.append(subel)
+def write_new_doc(xml_doc: TeiReader, docpath):
     docname = docpath.split("/")[-1].removesuffix(".xml") + "_pos.xml"
     new_docpath = f"{output_dir}/{docname}"
     print(f"writing {new_docpath}")
     xml_doc.tree_to_file(
         new_docpath
     )
+
+def tag_doc(docpath: str, source_elements_xpath: str):
+    # load doc
+    print(f"processing {docpath}")
+    xml_doc = TeiReader(docpath)
+    # identifiy text to annotate: source elements
+    source_elements = xml_doc.any_xpath(
+        source_elements_xpath
+    )
+    for source_element in source_elements:
+        if source_element.text is not None:
+            tokens = nl_processor(source_element.text)
+            source_element.text = "\n"
+            for token in tokens:
+                if token.pos_ == "PUNCT":
+                    subel = teiMaker.pc(
+                        token.text,
+                        pos=token.pos_
+                    )
+                else:
+                    subel = teiMaker.w(
+                        token.text,
+                        pos=token.pos_,
+                        lemma=token.lemma_,
+                        ana=token.morph.__str__()
+                    )
+                source_element.append(subel)
+    write_new_doc(xml_doc, docpath)
+
+if __name__ == "__main__":
+    for docpath in glob.glob(f"{editions_dir}/*.xml"):
+        tag_doc(docpath, source_elements_xpath)
